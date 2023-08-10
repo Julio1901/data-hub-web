40ac90a88e7a23d0f88c006c37c541112b844564
Julio Cesar <juliocesar@Julios-MacBook-Pro.local>
2023-08-10 19:29:15 -0300

    Login form created

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,18 +1,29 @@
-import { useNavigate } from "react-router-dom"; 
+import { useState } from "react";
 
-export const Login = () => {
+export const Login = () => {   
 
-    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    // Segundo modo de fazer navegação
-    const handleClick = () => {
-        navigate("/initial-page");
-    }
+    const handleLogin = () => {
+        console.log(email);
+        console.log(password);
+    };
 
     return (
         <div>
-            Login
-            <button onClick={handleClick}>Initial page</button>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)} / >
+                </label>
+                <label>
+                    <span>Password</span>
+                    <input type="password"  value={password} onChange={e => setPassword(e.target.value)} / >
+                </label>
+                <button type="button" onClick={handleLogin}>Login</button>
+            </form>  
+          
         </div>
     );
 }
