c52a869f754b2d1cbbf1ed859bc5da6a9e601965
Julio Cesar <juliocesar@Julios-MacBook-Pro.local>
2023-08-25 00:04:27 -0300

    class 13

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,20 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 export const Dashboard = () => {
+
+    //Criando um objeto que tem o parâmetro counter do tipo numérico
+    const counterRef = useRef({ counter: 0 });
+
+
     return (
         <>
             <p>Dashboard</p>
+
+            <p>Contador: {counterRef.current.counter}</p>
+
+            <button  onClick={ () => counterRef.current.counter ++ }>Somar</button>
+            
             {/* Primeiro modo de fazer navegação */}
             <Link to="/login">Login</Link>
         </>
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { InputLogin } from "./components/InputLogin";
 
 export const Login = () => {   
+
+    const  inputPasswordRef = useRef<HTMLInputElement>(null);
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -11,23 +14,33 @@ export const Login = () => {
     const handleLogin = () => {
         console.log(email);
         console.log(password);
+        
+        if(inputPasswordRef.current != null) {
+            inputPasswordRef.current.focus()
+        }
     };
 
     return (
         <div>
             <form>
                 <p>Quantidade de caracteres no email: {emailLength}</p>
-                <label>
-                    <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)} / >
-                </label>
-                <label>
-                    <span>Password</span>
-                    <input type="password"  value={password} onChange={e => setPassword(e.target.value)} / >
-                </label>
-                <button type="button" onClick={handleLogin}>Login</button>
-            </form>  
-          
+             
+                <InputLogin 
+                label="Email" 
+                value={email} 
+                onChange={newValue => setEmail(newValue) }
+                onPressEnter={() => inputPasswordRef.current?.focus()}
+                />
+
+                <InputLogin 
+                label="Password" 
+                type="password"
+                value={password}
+                ref={inputPasswordRef}
+                onChange={newValue => setPassword(newValue)}
+                />
+            <button type="button" onClick={handleLogin}>Log in</button>
+            </form>           
         </div>
     );
 }
