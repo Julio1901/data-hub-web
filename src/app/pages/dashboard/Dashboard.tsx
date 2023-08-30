ce87a01bca39bb9b4756d139826eef05c44a90df
Julio Cesar <juliocesar@Julios-MacBook-Pro.local>
2023-08-29 23:18:36 -0300

    Background image and principal container added into login page

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import { Title } from "./components/title";
+import * as S from './styles'
 
 
 
 export const Dashboard = () => {
     return (
-        <>
+        <S.ContainerPrincipal>
+
+            <S.ContainerPrincipalContent/>
             <Title titleContent="Data Hub "/>
             
             {/* Primeiro modo de fazer navegação */}
-            <Link to="/login">Login</Link>
-        </>
+            {/* <Link to="/login">Login</Link> */}
+        </S.ContainerPrincipal>
     );
 }
